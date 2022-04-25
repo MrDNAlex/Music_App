cf4be17e3947738c6ff781ac64042cde9299da8a
MrDNAlex <Mr.DNAlex.2003@gmail.com>
2022-04-25 19:31:52 -0400

    idk

---
+++ app/src/main/java/com/example/dnamusicapp/SongClass.java
@@ -0,0 +1,78 @@
+package com.example.dnamusicapp;
+
+public class SongClass {
+    //Master
+    /*Song Name
+    Artist Name
+    Album Name
+    Song Path
+    Album Art
+    Universal Rank
+    Universal Mood
+    Universal Tag
+    Song Length
+    Universal Times Played
+    Universal Total Time Played
+    Last Time Played
+    Album ID
+    Artist ID
+    Active Skin ID
+    File Song ID
+*/
+
+    //Playlist
+    /*
+    Song Name
+    Artist Name
+    Album Name
+    Song Path
+    Album Art
+    Playlist Rank
+    Universal Rank
+    Playlist Music Mood
+    Universal Music Mood
+    Playlist Music Tags
+    Universal Music Tags
+    Song Length
+    Playlist Times Played
+    Playlist Total Time Played
+    Last Time Played
+            AlbumID
+    ArtistID
+    Active Skin ID
+    File Song ID
+*/
+
+    //Extras
+    /*
+    BackColour
+    Fav Start
+    Fav End
+*/
+
+    boolean Universal;
+    String SongName;
+    String ArtistName;
+    String AlbumName;
+    String SongPath;
+    String AlbumArt;
+    int UniversalRank;
+    int PlaylistRank;
+    String UniversalMood; //Make this into array probably
+    String UniversalTag;
+    String PlaylistMood;
+    String PlaylistTag;
+    int SongLength;
+    int UniversalTimesPlayed;
+    int UniversalTimePlayed;
+    int PlaylistTimesPlayed;
+    int PlaylistTimePlayed;
+    int LastTimePlayed;
+    String AlbumID; //Maybe int
+    String ArtistID; //Maybe int
+
+
+
+
+
+}
